ea87a9dd8e623855aff0e4e0a20bdc1e0a54f3f7
2502429394 <2502429394@qq.com>
2023-03-28 19:27:16 +0800

    初次提交

---
+++ src/main/java/com/example/drup/DrupApplication.java
@@ -0,0 +1,13 @@
+package com.example.drup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/drup/DrupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
